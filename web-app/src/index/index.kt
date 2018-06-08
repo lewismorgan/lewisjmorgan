6ceeffc17b618de8edce569005c828c27610345b
Lewis <lewismorgan1995@icloud.com>
2018-06-08 19:36:53 -0400

    Update packages

--- web-app/src/index/index.kt
+++ web-app/src/index/index.kt
@@ -5,12 +5,14 @@ import com.lewismorgan.web.github.githubApp
 import kotlinext.js.*
 import react.dom.*
 import kotlin.browser.*
+import kotlin.js.RegExp
 
 fun main(args: Array<String>) {
-  requireAll(require.context("src", true, js("/\\.css$/")))
-
-  render(document.getElementById("root")) {
-    app()
-    githubApp()
+  requireAll(require.context("src", true, js("/\\.css$/") as RegExp))
+  window.onload = {
+    render(document.getElementById("root")!!) {
+      app()
+      githubApp()
+    }
   }
 }
