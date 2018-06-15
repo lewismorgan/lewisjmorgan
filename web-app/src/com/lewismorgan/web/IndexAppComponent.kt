585ccb053971dbf2ffa0fc1e51dbda42531db4d0
Lewis <lewismorgan1995@icloud.com>
2018-06-15 19:17:11 -0400

    Begin website redesign

--- web-app/src/com/lewismorgan/web/IndexAppComponent.kt
+++ web-app/src/com/lewismorgan/web/IndexAppComponent.kt
@@ -1,16 +1,96 @@
 package com.lewismorgan.web
 
-import react.*
-import react.dom.*
+import kotlinx.html.BUTTON
+import kotlinx.html.ButtonType
+import kotlinx.html.DIV
+import kotlinx.html.LI
+import kotlinx.html.MAIN
+import kotlinx.html.OL
+import kotlinx.html.attributesMapOf
+import kotlinx.html.id
+import kotlinx.html.li
+import react.RBuilder
+import react.RComponent
+import react.RProps
+import react.RState
+import react.ReactElement
+import react.dom.RDOMBuilder
+import react.dom.a
+import react.dom.body
+import react.dom.button
+import react.dom.div
+import react.dom.footer
+import react.dom.h1
+import react.dom.h3
+import react.dom.header
+import react.dom.li
+import react.dom.nav
+import react.dom.ol
+import react.dom.p
+import react.dom.span
+import react.dom.tag
+import react.dom.ul
 
 class IndexAppComponent : RComponent<RProps, RState>() {
   override fun RBuilder.render() {
-    div("App-header") {
-      h2 {
-        +"Welcome to my website"
+    // TODO Create HeaderComponent
+    nav("navbar navbar-expand-md navbar-dark fixed-top bg-dark") {
+      a(classes = "navbar-brand", href = "#") { +"Lewis' Temple" }
+      navbarToggler("navbar-toggler") {
+        span("navbar-toggler-icon") {}
+      }
+      // TODO Add id to div
+      div("collapse navbar-collapse") {
+        ul("navbar-nav mr-auto") {
+          li("nav-item active") {
+            a(classes = "nav-link", href = "#") { +"Home (current)" }
+          }
+          li("nav-item") {
+            a(classes = "nav-link", href = "#") { +"Projects" }
+          }
+        }
+      }
+    }
+
+    main("main", "main") {
+      // TODO Create a Carousel Component, refactor this out
+      carousel {
+        ol("carousel-indicators") {
+          carouselItem("active", "0") {}
+          carouselItem(slideTo = "1") {}
+          li("active") {
+            attrs["data-target"] = "#myCarousel"
+            attrs["data-slide-to"] = "0"
+          }
+        }
+        div("carousel-inner") {
+          div("carousel-item active") {
+            div("container") {
+              div("carousel-caption text-left") {
+                h1 { +"Welcome." }
+                p { +"TODO: Insert witty welcoming text here." }
+              }
+            }
+          }
+        }
       }
     }
   }
 }
 
-fun RBuilder.indexAppComponent() = child(IndexAppComponent::class) {}
+inline fun RBuilder.carouselItem(classes: String? = null, slideTo: String, block: RDOMBuilder<LI>.() -> Unit): ReactElement = tag(block) {
+  LI(attributesMapOf("class", classes, "data-target", "#myCarousel", "data-slide-to", slideTo), it)
+}
+
+
+inline fun RBuilder.main(classes: String? = null, role: String? = null, block: RDOMBuilder<MAIN>.() -> Unit): ReactElement = tag(block) { MAIN(attributesMapOf("class", classes, "role", role), it) }
+inline fun RBuilder.navbarToggler(classes: String? = null, block: RDOMBuilder<BUTTON>.() -> Unit): ReactElement = tag(block) {
+  BUTTON(attributesMapOf(
+      "class", classes, "type", "button", "data-toggle", "collapse",
+      "data-target", "#navbarCollapse", "aria-controls", "navbarCollapse", "aria-expanded", "false",
+      "aria-label", "Toggle Nav"), it)
+}
+
+inline fun RBuilder.carousel(block: RDOMBuilder<DIV>.() -> Unit): ReactElement = tag(block) {
+  DIV(attributesMapOf("class", "carousel slide", "data-ride", "carousel"), it)
+}
--- web-app/src/index/index.kt
+++ web-app/src/index/index.kt
@@ -1,15 +1,20 @@
 package index
 
-import com.lewismorgan.web.indexAppComponent
-import com.lewismorgan.web.github.githubApp
-import kotlinext.js.*
+import com.lewismorgan.web.IndexAppComponent
+import kotlinext.js.require
+import kotlinext.js.requireAll
 import react.dom.render
+import react.dom.script
 import kotlin.browser.document
 
 fun main(args: Array<String>) {
+  @Suppress("UnsafeCastFromDynamic")
+  require("bootstrap/dist/css/bootstrap.min.css")
   requireAll(require.context("src", true, js("/\\.css$/")))
   render(document.getElementById("root")) {
-    indexAppComponent()
-    githubApp()
+    child(IndexAppComponent::class) {}
+    script(src = "jquery/dist/jquery.js") {}
+    script(src = "popper.js/dist/popper.js") {}
+    script(src = "bootstrap/dist/js/bootstrap.min.js") {}
   }
 }
