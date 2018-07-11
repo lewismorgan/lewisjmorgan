7654a0439004a09cd66c90bb540cda726e8fbe45
Lewis <lewismorgan1995@icloud.com>
2018-07-10 23:30:57 -0400

    Use gulp for pre-build tasks

---
+++ web-app/gulpfile.js
@@ -0,0 +1,38 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var cleanCSS = require('gulp-clean-css');
+var del = require('del');
+
+const paths = {
+  styles: {
+    src: 'scss/**/*.scss',
+    dest: 'src/stylesheets'
+  }
+};
+
+function clean() {
+  return del(['src/stylesheets']);
+}
+
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(sass())
+    .pipe(cleanCSS())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+function watch() {
+  gulp.watch(paths.styles.src, styles);
+}
+
+gulp.task('clean', clean);
+gulp.task('styles', styles);
+gulp.task('watch', watch);
+
+var build = gulp.series(clean, styles);
+gulp.task('build', build);
+gulp.task('default', build);
