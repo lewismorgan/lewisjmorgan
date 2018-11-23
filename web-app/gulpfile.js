4b28e10727b8e3ac9b3329190ab00ad5d0119ea8
Lewis <lewismorgan1995@icloud.com>
2018-11-22 19:13:01 -0500

    Update stylesheet processing

--- web-app/gulpfile.js
+++ web-app/gulpfile.js
@@ -1,34 +1,47 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var rename = require('gulp-rename');
-var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 
 const paths = {
   styles: {
-    src: 'scss/**/*.scss',
-    dest: 'src/stylesheets'
+    src: 'stylesheets/**/*.scss',
+    dest: 'public/css'
   }
 };
 
+const sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
 function clean() {
-  return del(['src/stylesheets']);
+  return del(paths.styles.dest);
 }
 
-function styles() {
+function stylesheets() {
   return gulp.src(paths.styles.src)
-    .pipe(sass())
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.styles.dest));
 }
 
 function watch() {
-  gulp.watch(paths.styles.src, styles);
+  gulp.watch(paths.styles.src, sass);
+}
+
+function prod() {
+  return gulp.src(paths.styles.src)
+    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(gulp.dest(paths.styles.dest));
 }
 
 gulp.task('clean', clean);
-gulp.task('styles', styles);
+gulp.task('sass', stylesheets);
 gulp.task('watch', watch);
+gulp.task('prod', prod);
 
-var build = gulp.series(clean, styles);
+var build = gulp.series(clean, stylesheets);
 gulp.task('build', build);
 gulp.task('default', build);
