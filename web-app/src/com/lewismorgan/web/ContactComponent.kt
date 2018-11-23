123ced8459ce30a944d164c1eca58d41b8b8bc6a
Lewis <1057112+lewismorgan@users.noreply.github.com>
2018-11-22 23:18:00 -0500

    Add twitter timeline (#17)
    * Add initial embedded twitter timeline code
    
    * Add timeline styling
    
    * Bump package version

--- web-app/src/com/lewismorgan/web/ContactComponent.kt
+++ web-app/src/com/lewismorgan/web/ContactComponent.kt
@@ -2,15 +2,30 @@ package com.lewismorgan.web
 
 
 import com.lewismorgan.web.bootstrap.dsl.container
+import com.lewismorgan.web.wrappers.TwitterTimelineEmbedComponent
+import com.lewismorgan.web.wrappers.TwitterTimelineEmbedProps
+import kotlinx.html.classes
 import react.RBuilder
 import react.RComponent
+import react.RHandler
 import react.RProps
 import react.RState
+import react.ReactElement
 
 class ContactComponent : RComponent<RProps, RState>() {
   override fun RBuilder.render() {
     container {
-      // TODO Add contact component
+      attrs.classes += "twitter-feed"
+      twitterTimelineEmbedded {
+        attrs.noFooter = true
+        attrs.autoHeight = true
+        attrs.sourceType = "widget"
+        attrs.screenName = "TheLewisMorgan"
+      }
     }
   }
 }
+
+
+fun RBuilder.twitterTimelineEmbedded(handler: RHandler<TwitterTimelineEmbedProps>): ReactElement =
+    child<TwitterTimelineEmbedProps, TwitterTimelineEmbedComponent>(handler)
