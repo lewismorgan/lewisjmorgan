11840c197010e93f09ef464f87b43c0dbad186cb
Lewis <lewismorgan1995@icloud.com>
2019-01-15 16:10:50 -0500

    Update devdependencies
    Removed react-twitter

--- web-app/src/com/lewismorgan/web/ContactComponent.kt
+++ web-app/src/com/lewismorgan/web/ContactComponent.kt
@@ -2,8 +2,6 @@ package com.lewismorgan.web
 
 
 import com.lewismorgan.web.bootstrap.dsl.container
-import com.lewismorgan.web.wrappers.TwitterTimelineEmbedComponent
-import com.lewismorgan.web.wrappers.TwitterTimelineEmbedProps
 import kotlinx.html.classes
 import react.RBuilder
 import react.RComponent
@@ -16,16 +14,6 @@ class ContactComponent : RComponent<RProps, RState>() {
   override fun RBuilder.render() {
     container {
       attrs.classes += "twitter-feed"
-      twitterTimelineEmbedded {
-        attrs.noFooter = true
-        attrs.autoHeight = true
-        attrs.sourceType = "widget"
-        attrs.screenName = "TheLewisMorgan"
-      }
     }
   }
 }
-
-
-fun RBuilder.twitterTimelineEmbedded(handler: RHandler<TwitterTimelineEmbedProps>): ReactElement =
-    child<TwitterTimelineEmbedProps, TwitterTimelineEmbedComponent>(handler)
